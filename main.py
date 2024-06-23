9b3a92240e8d019194625a1d35d0db780d64062e
Shilo <shilo86@gmail.com>
2024-06-22 20:38:27 -0700

    feat: dynamically loading external phrases and words

--- main.py
+++ main.py
@@ -3,40 +3,29 @@ from random import choice
 import string
 
 FIXED_FIRST_WORD: bool = True
-PHRASES: list[str] = [
-    "You know what they {}, {}, {}.",
-    "You know what they say, a {} {} is a {} {}.",
-    "A {} penny is {} and {} {}.",
-    "A {} {} is a {} {}.",
-    "A {} {} is a {} towards a {}.",
-    "A {} {} is a {} you can {} again.",
-    "A wise {} {} is a wise {} {}.",
-    "Better a small {} than an empty {}."
-]
-WORDS: list[str] = [
-    "penny",
-    "gained",
-    "saved",
-    "earned",
-    "given",
-    "real",
-    "pennied",
-    "say",
-    "said",
-    "walked",
-    "eaten",
-    "pretty",
-    "penniless",
-    "dream",
-    "lose"
-]
+PHRASES_FILE_PATH: str = "data/phrases.txt"
+WORDS_FILE_PATH: str = "data/words.txt"
+DATA_ERROR_FALLBACK: str = "pennyless"
+
+
+def load_data(filename) -> list[str]:
+    try:
+        with open(filename, "r") as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        return [DATA_ERROR_FALLBACK]
+
 
 app: FastAPI = FastAPI()
+phrases: list[str] = load_data(PHRASES_FILE_PATH)
+words: list[str] = load_data(WORDS_FILE_PATH)
+
 
 @app.get("/penny-saver")
 @app.get("/penny-saver/")
 async def translate():
-    phrase: str = choice(PHRASES)
+    phrase: str = choice(phrases)
     return phrase.format(*random_substitution_words(phrase))
 
 
@@ -49,7 +38,7 @@ def random_substitution_words(text: str) -> list[str]:
         if field_name is None:
             continue
 
-        random_words.append(WORDS[0] if fixed_word else choice(WORDS))
+        random_words.append(words[0] if fixed_word else choice(words))
         fixed_word = False
 
     return random_words
